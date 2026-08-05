6fc984facec6aeaea5241a5d99e9011ccc26ccb4
GauravSB07 <gauravsb747@gmail.com>
2026-08-05 14:16:19 +0530

    Add NGO homepage structure, responsive navbar, and hover animations

---
+++ includes/navbar.php
@@ -0,0 +1,115 @@
+<nav class="navbar navbar-expand-lg bg-white shadow-sm py-3">
+
+<div class="container-fluid px-5">
+
+<a class="navbar-brand fw-bold" href="#">
+<img src="images/logo.png" height="70">
+<p>Sevartha Foundation</p>
+</a>
+
+<button class="navbar-toggler"
+data-bs-toggle="collapse"
+data-bs-target="#navbarNav">
+
+<span class="navbar-toggler-icon"></span>
+
+</button>
+
+<div class="collapse navbar-collapse justify-content-end"
+id="navbarNav">
+
+<ul class="navbar-nav align-items-center">
+
+<li class="nav-item dropdown position-static">
+
+<a class="nav-link fw-semibold px-4 dropdown-toggle"
+href="#"
+data-bs-toggle="dropdown">
+
+ABOUT US
+
+</a>
+
+<div class="dropdown-menu mega-menu p-4 border-0 shadow">
+
+<div class="row">
+
+<div class="col-md-4">
+
+<a href="#" class="dropdown-item">Home</a>
+
+<a href="#" class="dropdown-item">People</a>
+
+<a href="#" class="dropdown-item">Governance</a>
+
+<a href="#" class="dropdown-item">Annual Reports</a>
+
+</div>
+
+<div class="col-md-4">
+
+<a href="#" class="dropdown-item">Overview</a>
+
+<a href="#" class="dropdown-item">Partners</a>
+
+<a href="#" class="dropdown-item">Media Mentions</a>
+
+</div>
+
+</div>
+
+</div>
+
+</li>
+
+<li class="nav-item">
+
+<a class="nav-link fw-semibold px-4"
+href="#">
+
+OUR WORK
+
+</a>
+
+</li>
+
+<li class="nav-item">
+
+<a class="nav-link fw-semibold px-4"
+href="#">
+
+GET INVOLVED
+
+</a>
+
+</li>
+
+<li class="nav-item ms-5">
+
+<a href="#"
+class="btn btn-primary rounded-pill px-4">
+
+LOGIN
+
+</a>
+
+</li>
+
+<li class="nav-item ms-2">
+
+<a href="#"
+class="btn btn-outline-dark rounded-pill px-4">
+
+DONATE
+
+</a>
+
+</li>
+
+</ul>
+
+</div>
+
+</div>
+
+</nav>
